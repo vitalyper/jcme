1a0f95ba1da3c31cd3479604abd6fe7f2ca830c7
vitalyper <vitaly@ufairsoft.com>
2011-10-19 21:13:57 -0400

    Minor fixes to run.bat

--- run.bat
+++ run.bat
@@ -7,7 +7,7 @@ call :CHECK_FOR_BINARY java || goto :EOF
 goto :RUN
 
 :CHECK_FOR_BINARY
-%1>null 2>&1
+%1>nul 2>&1
 if ERRORLEVEL 9009 (
 	echo %1 is not found on your PATH
 )
@@ -16,7 +16,7 @@ goto :EOF
 :RUN
 set sc=jcmegen\src\jcmegen\main.clj 
 echo Starting clojure script %sc%
-java -cp .\jcmegen\lib\*;.\cmegen\src clojure.main %sc% --input-file config.clj
+java -cp .\jcmegen\lib\*;.\jcmegen\src clojure.main %sc% --input-file config.clj
 
 :EOF
 popd
